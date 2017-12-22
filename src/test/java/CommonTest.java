fa605fa223ccb1ec4807aa3bc9c84f65e21beba2
Hachel <fanhq3@asiainfo.com>
2017-12-22 15:01:52 +0800

    gitignore

---
+++ src/test/java/CommonTest.java
@@ -0,0 +1,14 @@
+import com.leetCode.problems.GenerateMatrix;
+import org.junit.Test;
+
+/**
+ * Created by Hachel on 2017/12/22
+ */
+public class CommonTest {
+
+    @Test
+    public void testMatrix(){
+        int[][] result = GenerateMatrix.generateMatrix(5);
+    }
+
+}
