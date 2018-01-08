9c5c37db3b346cc1d8353758cba895e1be5f549c
Hachel <fanhq3@asiainfo.com>
2018-01-08 16:17:41 +0800

    free style

--- src/main/java/com/leetCode/problems/SortLogin.java
+++ src/main/java/com/leetCode/problems/SortLogin.java
@@ -1,6 +1,13 @@
 package com.leetCode.problems;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.util.Comparator;
 import java.util.TreeSet;
 
@@ -9,13 +16,35 @@ import java.util.TreeSet;
  */
 public class SortLogin {
 
-    private static final String path ="";
+    private static final Logger logger = LoggerFactory.getLogger(SortLogin.class);
+
+    private static final String path ="D:\\subline text\\sql";
 
     public static void main(String[] args) {
 
         File file = new File(path);
+        try {
+            StringBuilder result = new StringBuilder();
+            //1
+            byte[] temp = new byte[1024];
+            FileInputStream fileInputStream = new FileInputStream(file);
+            while (fileInputStream.read(temp) != -1){
+                result.append(new String(temp));
+            }
+            System.out.println(result);
+            //1
+            result.setLength(0);
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null){
+                result.append(line);
+            }
+            System.out.println(result);
+        }catch (Exception e){
+            logger.error("", e);
+        }
 
-        TreeSet<SortString> treeSet = new TreeSet<SortString>(new Comparator<SortString>() {
+        TreeSet<SortString> treeSets = new TreeSet<SortString>(new Comparator<SortString>() {
             public int compare(SortString o1, SortString o2) {
                 if (o1.getCount() < o2.getCount()){
                     return -1;
