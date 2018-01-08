568bcf101e9880dde098766c63dc9b3e5f8068fd
Hachel <fanhq3@asiainfo.com>
2018-01-08 15:47:24 +0800

    free style

--- src/main/java/com/leetCode/problems/Application.java
+++ src/main/java/com/leetCode/problems/Application.java
@@ -1,25 +1,15 @@
 package com.leetCode.problems;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 /**
  * Created by Hachel on 2018/1/2
  */
 public class Application {
 
+
     public static void main(String[] args) {
-        Map<String,String> map = new LinkedHashMap<String, String>();
-        map.put("1","1");
-        map.put("2","2");
-        map.put("3","3");
 
 
-
-        map.get("2");
-        for (Map.Entry entry : map.entrySet()){
-            System.out.println(entry.getValue());
-        }
     }
 
 }
