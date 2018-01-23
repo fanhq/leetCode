506dc7a4e7e095d6b7300795098f62f0cc60542a
Hachel <fanhq3@asiainfo.com>
2018-01-23 14:18:09 +0800

    free style

--- src/main/java/com/leetCode/zkCurator/DiscoveryClient.java
+++ src/main/java/com/leetCode/zkCurator/DiscoveryClient.java
@@ -3,43 +3,52 @@ package com.leetCode.zkCurator;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.curator.x.discovery.ServiceDiscovery;
-import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
 import org.apache.curator.x.discovery.ServiceInstance;
-import org.omg.CORBA.ServiceDetail;
 
-import java.util.Collection;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Hachel on 2018/1/23
  */
 public class DiscoveryClient {
 
+    public static final String BASE_PATH = "services";
+    public static final String SERVICE_NAME = "HelloService";
 
     public static void main(String[] args) {
 
+        CuratorFramework client = null;
+        ServiceDiscover serviceDiscover = null;
         try {
-            CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", new ExponentialBackoffRetry(1000, 3));
+            client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", new ExponentialBackoffRetry(1000, 3));
+            ;
             client.start();
-            client.blockUntilConnected();
 
-            ServiceDiscovery<ServiceDetail> serviceDiscovery = ServiceDiscoveryBuilder.builder(ServiceDetail.class)
-                    .client(client)
-                    .basePath("/ai/obc/")
-                    .build();
-            serviceDiscovery.start();
+            serviceDiscover = new ServiceDiscover(client, DiscoveryClient.BASE_PATH);   //服务发现
+            serviceDiscover.start();
 
-            //根据名称获取服务
-            Collection<ServiceInstance<ServiceDetail>> services = serviceDiscovery.queryForInstances("iop");
-            for(ServiceInstance<ServiceDetail> service : services) {
-                System.out.println(service.getPayload());
-                System.out.println(service.getAddress() + "\t" + service.getPort());
+            for (int i = 0; i < 10; i++) {
+
+                ServiceInstance<ServerPayload> instance = serviceDiscover.getServiceProvider(DiscoveryClient.SERVICE_NAME);
+
+                System.out.println("service:" + DiscoveryClient.SERVICE_NAME + " instance id:" + instance.getId() +
+                        ", name:" + instance.getName() + ", address:" + instance.getAddress() + ", port:" + instance.getPort());
+
+                TimeUnit.SECONDS.sleep(3);
             }
 
-            serviceDiscovery.close();
-            client.close();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (serviceDiscover != null) {
+                try {
+                    serviceDiscover.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            client.close();
         }
     }
 
---
+++ src/main/java/com/leetCode/zkCurator/ServerPayload.java
@@ -0,0 +1,35 @@
+package com.leetCode.zkCurator;
+
+/**
+ * Created by Hachel on 2018/1/23
+ */
+public class ServerPayload {
+
+    private String cluster;
+    private int payload;
+
+    public ServerPayload(){
+
+    }
+
+    public ServerPayload(String cluster, int payload) {
+        this.cluster = cluster;
+        this.payload = payload;
+    }
+
+    public String getCluster() {
+        return cluster;
+    }
+
+    public void setCluster(String cluster) {
+        this.cluster = cluster;
+    }
+
+    public int getPayload() {
+        return payload;
+    }
+
+    public void setPayload(int payload) {
+        this.payload = payload;
+    }
+}
