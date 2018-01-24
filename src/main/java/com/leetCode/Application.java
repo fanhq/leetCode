2770db03a2eb440b10acce6a9f29911ff9291047
Hachel <fanhq3@asiainfo.com>
2018-01-24 14:04:25 +0800

    free style

--- src/main/java/com/leetCode/Application.java
+++ src/main/java/com/leetCode/Application.java
@@ -12,7 +12,9 @@ public class Application {
     public static void main(String[] args) {
         SimpleDateFormat DATA_FORMAT_YYYY_MM_DD_HH_MM_SS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String crmQueryTime = DATA_FORMAT_YYYY_MM_DD_HH_MM_SS.format(new Date());
-        System.out.println(crmQueryTime);
+       // System.out.println(crmQueryTime);
+
+        System.out.println(1<2-3);
     }
 
 }
