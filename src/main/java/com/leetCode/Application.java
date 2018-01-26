1575422354055d81516d75b4df601815d58c1eaf
Hachel <fanhq3@asiainfo.com>
2018-01-26 16:29:10 +0800

    free style

--- src/main/java/com/leetCode/Application.java
+++ src/main/java/com/leetCode/Application.java
@@ -1,20 +1,20 @@
 package com.leetCode;
 
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * Created by Hachel on 2018/1/2
  */
 public class Application {
 
     public static void main(String[] args) {
-        SimpleDateFormat DATA_FORMAT_YYYY_MM_DD_HH_MM_SS = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String crmQueryTime = DATA_FORMAT_YYYY_MM_DD_HH_MM_SS.format(new Date());
-       // System.out.println(crmQueryTime);
 
-        System.out.println(1<2-3);
+        Thread.currentThread().interrupt();
+        System.out.println(Thread.currentThread().isInterrupted());
+        Thread.currentThread().interrupted();
+        System.out.println(Thread.currentThread().isInterrupted());
+
+        System.out.println("end");
+
     }
 
 }
--- src/main/java/com/leetCode/guava/MyGuava.java
+++ src/main/java/com/leetCode/guava/MyGuava.java
@@ -1,4 +1,4 @@
-package com.leetCode.Guava;
+package com.leetCode.guava;
 
 /**
  * Created by Hachel on 2018/1/23
