c939a6f2c05af9199f941e9e1f577284ad3ae936
Hachel <fanhq3@asiainfo.com>
2018-02-02 12:39:14 +0800

    free style

--- src/main/java/com/leetCode/Application.java
+++ src/main/java/com/leetCode/Application.java
@@ -1,17 +1,24 @@
 package com.leetCode;
 
 
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created by Hachel on 2018/1/2
  */
 public class Application {
 
-    private final static char DEFAULT_SPLIT_KEY = 1;
 
     public static void main(String[] args) {
-        String msgs = "10|13999999999|msg|";
-        String[] s = msgs.split("\\|");
-        System.out.println(s.length);
+        try {
+            InputStream is = Application.class.getClassLoader().getResourceAsStream("application.properties");
+            Properties properties = new Properties();
+            properties.load(is);
+            System.out.println(properties.getProperty("name"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
