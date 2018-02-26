155fe9e475b3fd488d5fc79e6797474d38d6dceb
Hachel <fanhq3@asiainfo.com>
2018-02-26 14:54:02 +0800

    free style

--- src/main/java/com/leetCode/Application.java
+++ src/main/java/com/leetCode/Application.java
@@ -1,6 +1,9 @@
 package com.leetCode;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Hachel on 2018/1/2
  */
@@ -8,6 +11,11 @@ public class Application {
 
 
     public static void main(String[] args) {
+        List<String> a = new ArrayList<>();
+        List<String> b = a;
+
+        a.add("1");
+        b.add("2");
 
     }
 
