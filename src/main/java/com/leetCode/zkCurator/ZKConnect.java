8d2276bf37cbf3c09ef1c40f53794e6b179600eb
Hachel <fanhq3@asiainfo.com>
2018-02-27 12:56:32 +0800

    free style

--- src/main/java/com/leetCode/zkCurator/ZKConnect.java
+++ src/main/java/com/leetCode/zkCurator/ZKConnect.java
@@ -18,12 +18,14 @@ public class ZKConnect {
         int sessionTimeout = 30000;
         try {
             ZooKeeper zk = new ZooKeeper(connectionString, sessionTimeout, null);
-            List<String> children = zk.getChildren("/iop", new Watcher() {
+            zk.addAuthInfo("digest", ":".getBytes());
+            List<String> children = zk.getChildren("/iop/rpc", new Watcher() {
                 public void process(WatchedEvent event) {
                     System.out.println("this is children node event");
                     System.out.println(event);
                 }
             });
+            System.out.println(children.size());
             for (String s : children) {
                 System.out.println(s);
             }
