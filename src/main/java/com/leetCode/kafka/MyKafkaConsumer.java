e9995071aba66d7cf73c71bb3d388872183e5317
Hachel <fanhq3@asiainfo.com>
2018-03-01 17:47:04 +0800

    free style

--- src/main/java/com/leetCode/kafka/MyKafkaConsumer.java
+++ src/main/java/com/leetCode/kafka/MyKafkaConsumer.java
@@ -15,13 +15,13 @@ public class MyKafkaConsumer {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "test");
+        props.put("group.id", "group-1");
         props.put("enable.auto.commit", "true");
         props.put("auto.commit.interval.ms", "1000");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Arrays.asList("foo", "bar"));
+        consumer.subscribe(Arrays.asList(MyKafkaProducer.TOPIC));
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records)
