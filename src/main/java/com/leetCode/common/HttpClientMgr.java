f4c645d0afac065a2bc78e39d675fca687d9b98d
Hachel <fanhq3@asiainfo.com>
2018-03-08 15:06:08 +0800

    free style

--- src/main/java/com/leetCode/common/HttpClientMgr.java
+++ src/main/java/com/leetCode/common/HttpClientMgr.java
@@ -3,15 +3,12 @@ package com.leetCode.common;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.conn.HttpClientConnectionManager;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Hachel on 2017/12/26
@@ -35,14 +32,14 @@ public class HttpClientMgr {
             // 设置连接池
             connMgr = new PoolingHttpClientConnectionManager();
             // 设置连接池大小
-            connMgr.setMaxTotal(256);
+            connMgr.setMaxTotal(16);
             // 单路由最大连接数
-            connMgr.setDefaultMaxPerRoute(256);
+            connMgr.setDefaultMaxPerRoute(64);
             //官方推荐使用这个来检查永久链接的可用性，而不推荐每次请求的时候才去检查
             connMgr.setValidateAfterInactivity(2000);
             RequestConfig.Builder configBuilder = RequestConfig.custom();
             // 设置连接超时
-            configBuilder.setConnectTimeout(1500);
+            configBuilder.setConnectTimeout(3000);
             // 设置读取超时
             configBuilder.setSocketTimeout(3000);
             // 设置从连接池获取连接实例的超时，不能太大，不要让大量请求堆积在获取连接处
@@ -111,46 +108,6 @@ public class HttpClientMgr {
         } catch (Exception e) {
             logger.error("releaseConnection failed!", e);
         }
-    }
-
-
-    /**
-     * 连接回收策略
-     */
-    public static class IdleConnectionMonitorThread extends Thread {
-
-        private final HttpClientConnectionManager connMgr;
-        private volatile boolean shutdown;
-
-        public IdleConnectionMonitorThread(HttpClientConnectionManager connMgr) {
-            super();
-            this.connMgr = connMgr;
-        }
-
-        @Override
-        public void run() {
-            try {
-                while (!shutdown) {
-                    synchronized (this) {
-                        wait(10000);
-                        // 关闭失效的连接
-                        connMgr.closeExpiredConnections();
-                        // 可选的, 关闭30秒内不活动的连接
-                        connMgr.closeIdleConnections(30, TimeUnit.SECONDS);
-                    }
-                }
-            } catch (InterruptedException ex) {
-                logger.error("连接回收失败", ex);
-            }
-        }
-
-        public void shutdown() {
-            shutdown = true;
-            synchronized (this) {
-                notifyAll();
-            }
-        }
-
     }
 
 }
