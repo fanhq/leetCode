47c99b37a10a1c3ab77d1b13e00f3fc67a9645ea
Hachel <fanhq3@asiainfo.com>
2018-03-08 16:52:57 +0800

    free style

--- src/main/java/com/leetCode/security/RSAMain.java
+++ src/main/java/com/leetCode/security/RSAMain.java
@@ -12,17 +12,17 @@ public class RSAMain {
 
         String publicKeyStr = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCDODM6pHfp9LPaC4/4UncFZ/CHXmZ1TwcKV4i392oC8GPh5cYCD8+awgDsguFL8itz22u80E5nELIuwFhvWvkfEoHhGMm05BNmrbea/WeS6CpS85uB7rV7lDx1e+3WIzM1A0IAeVep+XoLcpTw5y3KSltKfqiwfMdk3GuE9pRWqQIDAQAB";
 
-        String filepath = "D:/temp file/";
-
-        RSAEncrypt.genKeyPair(filepath);
+//        String filepath = "D:/temp file/";
+//
+//        RSAEncrypt.genKeyPair(filepath);
 
         System.out.println("--------------公钥加密私钥解密过程-------------------");
         String plainText = "公钥加密私钥解密0000001";
         //公钥加密过程
-        byte[] cipherData = RSAEncrypt.encrypt(publicKeyStr, plainText.getBytes());
+        byte[] cipherData = RSAUtil.encrypt(publicKeyStr, plainText.getBytes());
         String cipher = Base64.encodeBase64String(cipherData);
         //私钥解密过程
-        byte[] res = RSAEncrypt.decrypt(privateKeyStr, Base64.decodeBase64(cipher));
+        byte[] res = RSAUtil.decrypt(privateKeyStr, Base64.decodeBase64(cipher));
         String restr = new String(res);
         System.out.println("原文：" + plainText);
         System.out.println("加密：" + cipher);
---
+++ src/main/java/com/leetCode/security/RSAUtil.java
@@ -0,0 +1,130 @@
+package com.leetCode.security;
+
+import org.apache.commons.codec.binary.Base64;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Hachel on 2018/3/8
+ */
+public class RSAUtil {
+
+    private final static Logger logger = LoggerFactory.getLogger(RSAUtil.class);
+
+    /**
+     * 密钥算法
+     */
+    public static final String ALGORITHM = "RSA";
+
+    /**
+     * 密钥长度
+     */
+    public static final int KEY_LENGTH = 1024;
+
+    /**
+     * 私钥解密过程
+     *
+     * @param privateKeyStr 私钥
+     * @param cipherData    密文数据
+     * @return 明文
+     * @throws Exception 解密过程中的异常信息
+     */
+    public static byte[] decrypt(String privateKeyStr, byte[] cipherData)
+            throws Exception {
+        if (privateKeyStr == null) {
+            throw new Exception("解密私钥为空, 请设置");
+        }
+        Cipher cipher = null;
+        try {
+            byte[] buffer = Base64.decodeBase64(privateKeyStr);
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(buffer);
+            KeyFactory keyFactory = KeyFactory.getInstance(ALGORITHM);
+            PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+            cipher = Cipher.getInstance("RSA");
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+            byte[] output = cipher.doFinal(cipherData);
+            return output;
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此解密算法");
+        } catch (InvalidKeyException e) {
+            throw new Exception("解密私钥非法,请检查");
+        } catch (IllegalBlockSizeException e) {
+            throw new Exception("密文长度非法");
+        } catch (BadPaddingException e) {
+            throw new Exception("密文数据已损坏");
+        }
+    }
+
+    /**
+     * 公钥加密过程
+     *
+     * @param publicKeyStr  公钥
+     * @param plainTextData 明文数据
+     * @return
+     * @throws Exception 加密过程中的异常信息
+     */
+    public static byte[] encrypt(String publicKeyStr, byte[] plainTextData)
+            throws Exception {
+        if (publicKeyStr == null) {
+            throw new Exception("加密公钥为空, 请设置");
+        }
+        Cipher cipher = null;
+        try {
+            byte[] buffer = Base64.decodeBase64(publicKeyStr);
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            X509EncodedKeySpec keySpec = new X509EncodedKeySpec(buffer);
+            PublicKey publicKey = keyFactory.generatePublic(keySpec);
+            cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] output = cipher.doFinal(plainTextData);
+            return output;
+        } catch (NoSuchAlgorithmException e) {
+            throw new Exception("无此加密算法");
+        } catch (InvalidKeyException e) {
+            throw new Exception("加密公钥非法,请检查");
+        } catch (IllegalBlockSizeException e) {
+            throw new Exception("明文长度非法");
+        } catch (BadPaddingException e) {
+            throw new Exception("明文数据已损坏");
+        }
+    }
+
+    /**
+     *
+     * @return
+     */
+    public static Map<String, String> genKeyPair() {
+        // KeyPairGenerator类用于生成公钥和私钥对，基于RSA算法生成对象
+        KeyPairGenerator keyPairGen = null;
+        try {
+            keyPairGen = KeyPairGenerator.getInstance(ALGORITHM);
+        } catch (NoSuchAlgorithmException e) {
+            logger.error("NoSuchAlgorithmException", e);
+        }
+        // 初始化密钥对生成器，密钥大小为96-1024位
+        keyPairGen.initialize(KEY_LENGTH, new SecureRandom());
+        // 生成一个密钥对，保存在keyPair中
+        KeyPair keyPair = keyPairGen.generateKeyPair();
+        // 得到私钥
+        PrivateKey privateKey = keyPair.getPrivate();
+        // 得到公钥
+        PublicKey publicKey = keyPair.getPublic();
+        Map<String, String> keyMap = new HashMap<>();
+        // 得到公钥字符串
+        keyMap.put("pubilcKey", Base64.encodeBase64String(publicKey.getEncoded()));
+        // 得到私钥字符串
+        keyMap.put("privateKey", Base64.encodeBase64String(privateKey.getEncoded()));
+
+        return keyMap;
+    }
+
+}
