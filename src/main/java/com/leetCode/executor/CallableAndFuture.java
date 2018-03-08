8124a847ad0fad8de8c82b27787578c7547117ec
Hachel <fanhq3@asiainfo.com>
2018-03-08 16:01:04 +0800

    free style

---
+++ src/main/java/com/leetCode/executor/CallableAndFuture.java
@@ -0,0 +1,47 @@
+package com.leetCode.executor;
+
+import java.util.concurrent.*;
+
+/**
+ * Created by Hachel on 2018/3/8
+ */
+public class CallableAndFuture {
+
+    static class RunableClass implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("runable thread");
+        }
+    }
+
+    /**
+     * Callable结合ExecutorService使用
+     */
+    static class CallableClass implements Callable<String> {
+        @Override
+        public String call() throws Exception {
+            System.out.println("callable thread");
+            return "success";
+        }
+    }
+
+    private static ExecutorService executor = Executors.newSingleThreadExecutor();
+
+    public static void main(String[] args) {
+        RunableClass runableClass = new RunableClass();
+        executor.submit(runableClass);
+
+        CallableClass callableClass = new CallableClass();
+        Future<String> future = executor.submit(callableClass);
+        try {
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        executor.shutdown();
+    }
+
+}
