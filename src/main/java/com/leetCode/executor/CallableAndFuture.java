6cf6af62b91955a66cc1d7e716000e3e2b1012c6
Hachel <fanhq3@asiainfo.com>
2018-05-27 15:54:43 +0800

    free style

--- src/main/java/com/leetCode/executor/CallableAndFuture.java
+++ src/main/java/com/leetCode/executor/CallableAndFuture.java
@@ -1,5 +1,7 @@
 package com.leetCode.executor;
 
+import com.google.common.util.concurrent.*;
+
 import java.util.concurrent.*;
 
 /**
@@ -21,6 +23,11 @@ public class CallableAndFuture {
         @Override
         public String call() throws Exception {
             System.out.println("callable thread");
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
             return "success";
         }
     }
@@ -29,12 +36,15 @@ public class CallableAndFuture {
 
     public static void main(String[] args) {
         RunableClass runableClass = new RunableClass();
+        System.out.println("submit runable");
         executor.submit(runableClass);
 
         CallableClass callableClass = new CallableClass();
+        System.out.println("submit callable");
         Future<String> future = executor.submit(callableClass);
 
         FutureTask<String> futureTask = new FutureTask<>(callableClass);
+        System.out.println("submit futureTask");
         executor.submit(futureTask);
         try {
             System.out.println(future.get());
@@ -44,6 +54,27 @@ public class CallableAndFuture {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+
+        // 使用guava提供的MoreExecutors工具类包装原始的线程池
+        ListeningExecutorService listeningExecutor = MoreExecutors.listeningDecorator(executor);
+        //向线程池中提交一个任务后，将会返回一个可监听的Future，该Future由Guava框架提供
+        ListenableFuture<String> lf = listeningExecutor.submit(callableClass);
+        //添加回调，回调由executor中的线程触发，但也可以指定一个新的线程
+        Futures.addCallback(lf, new FutureCallback<String>() {
+
+            //耗时任务执行失败后回调该方法
+            @Override
+            public void onFailure(Throwable t) {
+                System.out.println("on failure" + t.getMessage());
+            }
+
+            //耗时任务执行成功后回调该方法
+            @Override
+            public void onSuccess(String s) {
+                System.out.println("on success " + s);
+            }
+        });
+
         executor.shutdown();
     }
 
