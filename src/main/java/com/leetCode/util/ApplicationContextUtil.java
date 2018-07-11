c41b03ca875b3f230dc767c2904a8eb3e5e66fa4
Hachel <fanhq3@asiainfo.com>
2018-07-11 10:13:18 +0800

    free style

---
+++ src/main/java/com/leetCode/util/ApplicationContextUtil.java
@@ -0,0 +1,44 @@
+package com.leetCode.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Hachel on 2018/7/11
+ */
+@Component
+public class ApplicationContextUtil implements ApplicationContextAware {
+    // Spring应用上下文环境
+    private static ApplicationContext applicationContext;
+
+    /**
+     * 实现ApplicationContextAware接口的回调方法，设置上下文环境
+     */
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        ApplicationContextUtil.applicationContext = applicationContext;
+    }
+
+    public static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    /**
+     * @return Object 一个以所给名字注册的bean的实例
+     * @throws BeansException
+     */
+    public static Object getBean(String beanName) throws BeansException {
+        return applicationContext.getBean(beanName);
+    }
+
+    /**
+     *
+     * @param beanClass
+     * @return
+     * @throws BeansException
+     */
+    public static Object getBean(Class beanClass) throws BeansException {
+        return applicationContext.getBean(beanClass);
+    }
+}
