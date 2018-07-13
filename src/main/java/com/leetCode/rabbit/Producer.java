99295e1ba1ec401a8be1fe033b18b9cb971e1f3d
Hachel <fanhq3@asiainfo.com>
2018-07-13 10:44:49 +0800

    free style

--- src/main/java/com/leetCode/rabbit/Producer.java
+++ src/main/java/com/leetCode/rabbit/Producer.java
@@ -26,9 +26,11 @@ public class Producer {
         Connection connection = factory.newConnection();
         //创建一个通道
         Channel channel = connection.createChannel();
+        // 声明fanout交换器 --direct（默认）、headers、fanout、topic
+        //channel.exchangeDeclare("fanoutec", "fanout");
         //  声明一个队列        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
         String message = "Hello RabbitMQ";
-        // 发送内容【参数说明：参数一：交换机名称；参数二：队列名称，参数三：消息的其他属性；参数四：消息主体】
+        // 发送内容【参数说明：参数一：交换机名称（路由键）；参数二：队列名称（绑定键），参数三：消息的其他属性；参数四：消息主体】
         channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
         System.out.println("Producer Send +'" + message + "'");
         //关闭通道和连接
