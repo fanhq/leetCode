50400493b4bf20ca03dac80d9251cad789c22e5b
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2018-08-30 16:59:56 +0800

    初始化

--- src/main/java/com/leetCode/Application.java
+++ src/main/java/com/leetCode/Application.java
@@ -1,9 +1,7 @@
 package com.leetCode;
 
 
-import java.net.URL;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -15,45 +13,20 @@ public class Application {
 
     private static ReentrantLock lock = new ReentrantLock();
 
-    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(1);
+    private static BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(128);
 
 
-   // private static ExecutorService executor = new ThreadPoolExecutor(4, 8, 0l, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>());
+    private static ExecutorService executor = new ThreadPoolExecutor(4, 8, 0l, TimeUnit.MILLISECONDS, queue);
 
     /**
      * @param args
      */
     public static void main(String[] args) {
         try {
-            String afterTransformValue = Integer.toBinaryString(Integer.valueOf("96", 16));
 
         } catch (Exception e) {
-            System.out.println("----------------");
             e.printStackTrace();
         }
-    }
-
-    public static String getAdd(String log, String lat) {
-        //lat 小  log  大
-        //参数解释: 纬度,经度 type 001 (100代表道路，010代表POI，001代表门址，111可以同时显示前三项)
-        String urlString = "http://gc.ditu.aliyun.com/regeocoding?l=" + lat + "," + log + "&type=010";
-        String res = "";
-        try {
-            URL url = new URL(urlString);
-            java.net.HttpURLConnection conn = (java.net.HttpURLConnection) url.openConnection();
-            conn.setDoOutput(true);
-            conn.setRequestMethod("POST");
-            java.io.BufferedReader in = new java.io.BufferedReader(new java.io.InputStreamReader(conn.getInputStream(), "UTF-8"));
-            String line;
-            while ((line = in.readLine()) != null) {
-                res += line + "\n";
-            }
-            in.close();
-        } catch (Exception e) {
-            System.out.println("error in wapaction,and e is " + e.getMessage());
-        }
-        System.out.println(res);
-        return res;
     }
 
 
