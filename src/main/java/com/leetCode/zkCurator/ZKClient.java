21aa754655e8921f9e1dfc8b1a84a7f098585aaa
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2018-10-31 14:56:25 +0800

    初始化

--- src/main/java/com/leetCode/zkCurator/ZKClient.java
+++ src/main/java/com/leetCode/zkCurator/ZKClient.java
@@ -1,5 +1,6 @@
 package com.leetCode.zkCurator;
 
+import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
@@ -11,18 +12,37 @@ import org.apache.zookeeper.CreateMode;
 public class ZKClient {
 
     public static void main(String[] args) throws Exception {
-        //start();
 
-        CuratorFramework client = null;
         try {
-            client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", new ExponentialBackoffRetry(1000, 3));
+            RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+            CuratorFramework client =
+                    CuratorFrameworkFactory.builder()
+                            .connectString("127.0.0.1")
+                            .sessionTimeoutMs(5000)
+                            .connectionTimeoutMs(5000)
+                            .retryPolicy(retryPolicy)
+                            .build();
             client.start();
-
-            client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath("/data/path", "hello".getBytes());
-
+            //创建节点
+            //PERSISTENT：持久化 默认模式
+            //PERSISTENT_SEQUENTIAL：持久化并且带序列号
+            //EPHEMERAL：临时
+            //EPHEMERAL_SEQUENTIAL：临时并且带序列号
+            //创建节点并递归创建父节点，并指定创建模式
+            client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath("/data/path", "hello".getBytes());
+            //获取节点数据
             byte[] data = client.getData().forPath("/data/path");
             System.out.println(new String(data));
-
+            //更新数据
+            client.setData().forPath("/data/path", "world".getBytes());
+            data = client.getData().forPath("/data/path");
+            System.out.println(new String(data));
+            //检查数据是否存在 返回等于null则不存在
+            client.checkExists().forPath("/data/path");
+            //删除数据
+            client.delete().deletingChildrenIfNeeded().forPath("/data/path");
+            data = client.getData().forPath("/data/path");
+            System.out.println(new String(data));
 
         } catch (Exception e) {
             e.printStackTrace();
