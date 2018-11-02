d47777be0f4aa8da1bb6150a01077a87d1cfcab7
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2018-11-02 14:08:22 +0800

    初始化

--- src/main/java/com/leetCode/zkCurator/ZKClient.java
+++ src/main/java/com/leetCode/zkCurator/ZKClient.java
@@ -3,6 +3,7 @@ package com.leetCode.zkCurator;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 
@@ -54,6 +55,22 @@ public class ZKClient {
             //获取子节点
             List<String> childs = client.getChildren().forPath("/");
             System.out.println(childs.size());
+
+            //创建zookeeper的客户端
+//            RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+//            CuratorFramework client = CuratorFrameworkFactory.newClient("10.21.41.181:2181,10.21.42.47:2181,10.21.49.252:2181", retryPolicy);
+//            client.start();
+
+            //创建分布式锁, 锁空间的根节点路径为/curator/lock
+            InterProcessMutex mutex = new InterProcessMutex(client, "/curator/lock");
+            mutex.acquire();
+            //获得了锁, 进行业务流程
+            System.out.println("Enter mutex");
+            //完成业务流程, 释放锁
+            mutex.release();
+
+            //关闭客户端
+            client.close();
 
         } catch (Exception e) {
             e.printStackTrace();
