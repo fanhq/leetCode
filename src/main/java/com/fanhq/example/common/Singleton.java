475aa7d3523b25174db36f456be8353d8e1f19c9
fanhaiqiu <fanhaiqiu@cmiot.chinamobile.com>
2019-04-15 15:44:23 +0800

    java 8 新特性

--- src/main/java/com/fanhq/example/common/Singleton.java
+++ src/main/java/com/fanhq/example/common/Singleton.java
@@ -21,6 +21,12 @@ public class Singleton {
         private static final Singleton INSTANCE = new Singleton();
     }
 
+    /**
+     * 当 Singleton 类加载时，静态内部类 SingletonHolder 没有被加载进内存。只有当调用 getUniqueInstance()方法从而触发
+     * SingletonHolder.INSTANCE 时 SingletonHolder 才会被加载，此时初始化 INSTANCE 实例，并且 JVM 能确保 INSTANCE
+     * 只被实例化一次
+     * @return
+     */
     public static Singleton getUniqueInstance() {
         return SingletonHolder.INSTANCE;
     }
