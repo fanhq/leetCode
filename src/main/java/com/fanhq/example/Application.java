53e468ffcd8d592d4d8a7f8f0a805233706770ae
fanhaiqiu <944080287@qq.com>
2020-01-06 19:58:06 +0800

    初始化

--- src/main/java/com/fanhq/example/Application.java
+++ src/main/java/com/fanhq/example/Application.java
@@ -5,20 +5,7 @@ package com.fanhq.example;
  * Created by Hachel on 2018/1/2
  */
 public class Application {
-
-    private static ThreadLocal<String> t1 = new ThreadLocal<>();
-    private static ThreadLocal<String> t2 = new ThreadLocal<>();
-    static {
-        t1.set("t1");
-        t2.set("t2");
-    }
-
     public static void main(String[] args) throws Exception {
-        System.out.println(t1.get());
-        System.out.println(t1.get());
-        System.out.println(t2.get());
-        System.out.println(t2.get());
+
     }
-
-
 }
