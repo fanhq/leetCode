811d1c7d6ba0a9da164846f0755bd758c4a49915
fanhaiqiu <944080287@qq.com>
2022-04-18 22:24:09 +0800

    init

---
+++ src/main/java/com/fanhq/example/netty/UdpClient.java
@@ -0,0 +1,46 @@
+package com.fanhq.example.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.UnpooledByteBufAllocator;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.DatagramPacket;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.net.InetSocketAddress;
+import java.nio.charset.Charset;
+
+/**
+ * @author: fanhaiqiu
+ * @date: 2022/4/18
+ */
+public class UdpClient  {
+
+    public static void main(String[] args) throws Exception{
+        EventLoopGroup group = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.group(group)
+                .option(ChannelOption.SO_BROADCAST, true)
+                .channel(NioDatagramChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new StringEncoder());
+                    }
+                });
+        ChannelFuture channelFuture = bootstrap.bind(0).sync();
+
+        InetSocketAddress remoteAddress = new InetSocketAddress("192.168.0.103", 6001);
+        ByteBuf byteBuf1 = new UnpooledByteBufAllocator(false).buffer();
+        byteBuf1.writeCharSequence("hello", Charset.forName("utf-8"));
+        DatagramPacket packet = new DatagramPacket(byteBuf1, remoteAddress);
+        channelFuture.channel().writeAndFlush(packet);
+
+        channelFuture.channel().closeFuture().sync();
+        group.shutdownGracefully();
+    }
+}
